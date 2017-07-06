563b2297f037ee0f2ca0996805410429727819cd
Himanshu Singh <himanshusingh2407@gmail.com>
2017-07-07 01:11:49 +0530

    Add Schedule section

--- scripts/main.js
+++ scripts/main.js
@@ -31,3 +31,13 @@ function initMap() {
 var redirectMap = function () {
   window.open('https://www.google.co.in/maps/place/JW+Marriott+Hotel+Bengaluru/@12.972006,77.5926823,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae1679c4616049:0x21e7060fdb88a82b!8m2!3d12.972006!4d77.594871', '_blank');
 }
+
+var toggleSchedule = function (day) {
+  if(day === 1) {
+    document.getElementById('schedule-day2').classList.add('hidden');
+    document.getElementById('schedule-day1').classList.remove('hidden');
+  } else {
+    document.getElementById('schedule-day1').classList.add('hidden');
+    document.getElementById('schedule-day2').classList.remove('hidden');
+  }
+}
